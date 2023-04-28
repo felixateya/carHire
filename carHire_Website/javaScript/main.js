14eab1ba6ff6b7ff6ab1692ce6da9d5b82428f0f
felixateya <122964383+felixateya@users.noreply.github.com>
2023-04-28 08:51:30 +0300

    Add files via upload

---
+++ carHire_Website/javaScript/main.js
@@ -0,0 +1,51 @@
+const D = new Date();
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    var uid = user.uid;
+    console.log(uid);
+
+    var userEmail = user.email;
+    console.log(userEmail);
+
+    firebase
+      .firestore()
+      .collection("Users")
+      .doc(uid)
+      .get()
+      .then((doc) => {
+        let userName = doc.data().userName;
+        let userEmail = doc.data().emailAddress;
+        // document.getElementById("userName").innerText =
+        //   "Welcome" + " " + userName;
+        // document.getElementById("email").innerText = userEmail;
+      });
+    // document.getElementById("logOut").onclick = function () {
+    //   firebase
+    //     .auth()
+    //     .signOut()
+    //     .then(() => {
+    //       // Sign-out successful.
+    //       window.location.href = "/login.html";
+    //     })
+    //     .catch((error) => {
+    //       // An error happened.
+    //     });
+    // };
+  } else {
+    window.location.href = "/login.html";
+  }
+});
+
+document.getElementById("book").onclick = function () {
+  window.location.href = "/cars.html";
+};
+
+function changeBackground() {
+  var myDiv = document.getElementById("myDiv");
+  if (window.scrollY > 0) {
+    myDiv.classList.add("change-background");
+  } else {
+    myDiv.classList.remove("change-background");
+  }
+}
